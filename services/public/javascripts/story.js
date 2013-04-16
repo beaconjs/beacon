f238b6979b91da695641abea4c1a8cafcb88b6c2
Aishwarya Singhal <asinghal79@gmail.com>
2013-04-16 14:31:25 +0530

    more styling

--- services/public/javascripts/story.js
+++ services/public/javascripts/story.js
@@ -2,3 +2,17 @@ function addStory() {
     $("#fade").show();
     $("#newStory").show();
 }
+
+function closePopup() {
+    $("#fade").hide();
+    $("#newStory").hide();
+    return false;
+}
+
+function openStory(id) {
+    $("#storyTitle").val('test test');
+    $("#storyDetails").val('test test test test test test test test test test');
+
+    $("#fade").show();
+    $("#newStory").show();
+}
