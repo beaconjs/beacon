a277a5a96956b52983e9710b397089cf0e3755d3
Aishwarya Singhal <asinghal79@gmail.com>
2013-04-18 12:15:04 +0530

    Update task.js

--- services/models/card_wall/task.js
+++ services/models/card_wall/task.js
@@ -1,7 +1,29 @@
+var db = require("../../db.js").sequelize;
+var DataTypes = require("sequelize");
+
 var Task = function( name) {
  this.name = name,
  this.created_at = new Date(),
  this.modified_at = new Date();
 };
 
-exports=Task;
+var tasks_table = function() {
+    return db.define('tasks', {
+      name: DataTypes.STRING,
+      created_by: DataTypes.INTEGER,
+      created_at: DataTypes.DATE,
+      modified_by: DataTypes.INTEGER,
+      modified_at: DataTypes.DATE
+    });
+};
+
+exports.Task=Task;
+exports.table=tasks_table;
+exports.save=function(task) {
+    var instance = tasks_table().build(task);
+    instance.save().success(function(){
+        console.log("saved");
+    }).error(function(error) {
+        console.log(error);
+    });
+};
