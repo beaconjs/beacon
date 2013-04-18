f452e15ba7a199307b6cfef497d8f078f91687d2
Aishwarya Singhal <asinghal79@gmail.com>
2013-04-18 12:10:37 +0530

    Update sprint.js

--- services/models/card_wall/sprint.js
+++ services/models/card_wall/sprint.js
@@ -1,10 +1,34 @@
+var db = require("../../db.js").sequelize;
+var DataTypes = require("sequelize");
+
 var Sprint = function(name, stories, startDate, endDate) {
  this.name = name, 
  this.stories = stories, 
- this.startDate = startDate, 
- this.endDate = endDate,
+ this.start_date = startDate, 
+ this.end_date = endDate,
  this.created_at = new Date(),
  this.modified_at = new Date();
 };
 
+var sprints_table = function() {
+    return db.define('sprints', {
+      name: DataTypes.STRING,
+      start_date: DataTypes.DATE,
+      end_date: DataTypes.DATE,
+      created_by: DataTypes.INTEGER,
+      created_at: DataTypes.DATE,
+      modified_by: DataTypes.INTEGER,
+      modified_at: DataTypes.DATE
+    });
+};
+
 exports=Sprint;
+exports.table=sprints_table;
+exports.save=function(sprint) {
+    var instance = sprints_table().build(sprint);
+    instance.save().success(function(){
+        console.log("saved");
+    }).error(function(error) {
+        console.log(error);
+    });
+};
