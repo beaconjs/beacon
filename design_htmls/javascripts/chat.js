19189ae7e828c81ded4ed5e21030c069033c0e8d
Aishwarya Singhal <asinghal79@gmail.com>
2013-04-28 07:39:29 +0530

    chat!

--- design_htmls/javascripts/chat.js
+++ design_htmls/javascripts/chat.js
@@ -1,11 +1,134 @@
 var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-function chat() {
+function chat(message) {
     var date = new Date(); 
     var d = date.getDate() + ' ' + months[date.getMonth()] + ' ' + date.getFullYear() + ' @ ' + date.getHours() + ':' + date.getMinutes();
     var msg = "";
-    msg += $('#chatMsg').val();
+    msg += message;
     msg += ' <span>' + d + "</span>";
     $('.chat-box').append("<div class='chat-row'>" + msg + "</div>");
     $('div.chat-box').scrollTo('div.chat-row:last');
 }
+
+var connection = null;
+
+var sendChat = function() {
+    var msg = $('#chatMsg').val();
+    if (!msg) {
+        return;
+    }
+    console.log(msg);
+    // send the message as an ordinary text
+    connection.send(msg);
+    $('#chatMsg').val('');
+    // disable the input field to make the user wait until server
+    // sends back response
+    //$('#chatMsg').attr('disabled', 'disabled');
+}
+
+
+var startChat = function () {
+    "use strict";
+ 
+    // for better performance - to avoid searching in DOM
+    var content = $('.chat-box');
+    var input = $('#chatMsg');
+    var status = $('.chat-box');
+ 
+    // my color assigned by the server
+    var myColor = false;
+    // my name sent to the server
+    var myName = 'test test';
+ 
+    // if user is running mozilla then use it's built-in WebSocket
+    window.WebSocket = window.WebSocket || window.MozWebSocket;
+ 
+    // if browser doesn't support WebSocket, just show some notification and exit
+    if (!window.WebSocket) {
+        content.html($('<p>', { text: 'Sorry, but your browser doesn\'t '
+                                    + 'support WebSockets.'} ));
+        input.hide();
+        $('span').hide();
+        return;
+    }
+ 
+    // open connection
+    connection = new WebSocket('ws://127.0.0.1:1337');
+
+    connection.onopen = function () {
+        // first we want users to enter their names
+        input.removeAttr('disabled');
+        status.text('Choose name:');
+    };
+ 
+    connection.onerror = function (error) {
+        // just in there were some problems with conenction...
+        content.html($('<p>', { text: 'Sorry, but there\'s some problem with your '
+                                    + 'connection or the server is down.' } ));
+    };
+ 
+    // most important part - incoming messages
+    connection.onmessage = function (message) {
+        // try to parse JSON message. Because we know that the server always returns
+        // JSON this should work without any problem but we should make sure that
+        // the massage is not chunked or otherwise damaged.
+        try {
+            var json = JSON.parse(message.data);
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ', message.data);
+            return;
+        }
+ 
+        // NOTE: if you're not sure about the JSON structure
+        // check the server source code above
+        if (json.type === 'color') { // first response from the server with user's color
+            myColor = json.data;
+            status.text(myName + ': ').css('color', myColor);
+            input.removeAttr('disabled').focus();
+            // from now user can start sending messages
+        } else if (json.type === 'history') { // entire message history
+            // insert every single message to the chat window
+            for (var i=0; i < json.data.length; i++) {
+                addMessage(json.data[i].author, json.data[i].text,
+                           json.data[i].color, new Date(json.data[i].time));
+            }
+        } else if (json.type === 'message') { // it's a single message
+            input.removeAttr('disabled'); // let the user write another message
+            addMessage(json.data.author, json.data.text,
+                       json.data.color, new Date(json.data.time));
+        } else {
+            console.log('Hmm..., I\'ve never seen JSON like this: ', json);
+        }
+    };
+ 
+    /**
+     * Send mesage when user presses Enter key
+     */
+    /*input.keydown(function(e) {
+        if (e.keyCode === 13) {
+            sendChat();
+        }
+    });*/
+ 
+    /**
+     * This method is optional. If the server wasn't able to respond to the
+     * in 3 seconds then show some error message to notify the user that
+     * something is wrong.
+     */
+    setInterval(function() {
+        if (connection.readyState !== 1) {
+            status.text('Error');
+            input.attr('disabled', 'disabled').val('Unable to comminucate '
+                                                 + 'with the WebSocket server.');
+        }
+    }, 3000);
+ 
+    /**
+     * Add message to the chat window
+     */
+    function addMessage(author, message, color, dt) {
+        chat(message);
+    }
+};
+
+startChat();
