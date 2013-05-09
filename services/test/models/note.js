2bcc62911897c7c5b446dd3e6a2bb23eef5f7bb5
Aishwarya Singhal <asinghal79@gmail.com>
2013-05-09 09:59:30 +0530

    more tests

--- services/test/models/note.js
+++ services/test/models/note.js
@@ -2,14 +2,16 @@ var expect = require('chai').expect,
     should = require('chai').should();
 
 var Note = require('../../models/note').get;
+var noteId = 1;
 
 var assert = require("assert")
 describe('Note', function(){
   describe('#save()', function(){
     it('should save a note', function(done){
-        var note = new Note('test note', 'this is a test note');
+        var note = new Note('test note', 'this is a test note', 1);
         note.save(function(o){
             expect(o.id).to.not.equal(null);
+            noteId = o.id;
             done();
         }, function(error){
             done();
@@ -17,4 +19,29 @@ describe('Note', function(){
         });
     })
   })
+
+  describe('#list()', function(){
+    it('should list notes for a project', function(done){
+        Note.all(1, function(o){
+            expect(o.length).to.not.equal(0);
+            done();
+        }, function(error){
+            done();
+            if (error) throw error;
+        });
+    })
+  })
+
+  describe('#get()', function(){
+    it('should get note details for a project', function(done){
+        Note.get(1, noteId, function(o){
+            expect(o.length).to.not.equal(0);
+            done();
+        }, function(error){
+            done();
+            if (error) throw error;
+        });
+    })
+  })
+
 })
