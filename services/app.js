8d1dece351828f73472d5935104a09d08a51bc5b
Aishwarya Singhal <asinghal79@gmail.com>
2013-05-21 00:55:21 +0530

    fixed CORS issue

--- services/app.js
+++ services/app.js
@@ -21,6 +21,15 @@ require('./notifications');
 
 var app = express();
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With, Content-Type, Accept, timeout, Content-Length, Accept-Encoding, Accept-Charset');
+
+    next();
+}
+
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
@@ -28,6 +37,7 @@ app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
+app.use(allowCrossDomain);
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
@@ -42,6 +52,9 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// handle OPTIONS requests from the browser
+app.options("*", function(req,res,next){res.send(200);});
+
 app.get('/', routes.index);
 app.post('/authenticate', user.authenticate);
 app.get('/users/search/:name', user.lookup);
@@ -52,6 +65,7 @@ app.get('/projects/:id', projects.get);
 app.get('/projects/:id/notes', notes.list);
 app.get('/projects/:id/notes/:noteId', notes.get);
 app.post('/projects/:id/:noteId/upload', notes.upload);
+app.post('/projects/:id/upload', projects.upload);
 
 app.get('/projects/:id/members', projectusers.list);
 app.post('/projects/:id/members', projectusers.create);
@@ -75,6 +89,9 @@ app.post('/projects/:id/sprints', cardModels.sprints.create);
 app.post('/projects/:id/lanes', cardModels.lanes.create);
 app.post('/epics/:id/stories', cardModels.stories.create);
 app.post('/stories/:id', cardModels.stories.update);
+
+app.get('/projects/:id/progress', cardModels.stories.progress);
+app.get('/projects/:id/:sprintId/progress', cardModels.stories.progress);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
