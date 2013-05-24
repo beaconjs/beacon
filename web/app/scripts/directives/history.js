f102fc86ebbc80e88248c3c0d5cbc57f2fa89fa3
Aishwarya Singhal <asinghal79@gmail.com>
2013-05-24 12:52:05 +0530

    improved formatting

--- web/app/scripts/directives/history.js
+++ web/app/scripts/directives/history.js
@@ -11,7 +11,18 @@ angular.module('webApp')
             var load = function() {
                 if ($rootScope.project_id) {
                     sync.get('/projects/' + $rootScope.project_id + '/notifications').success(function(res){
-                        $rootScope.allNotifications = res || [];
+                        var r = res || [];
+                        var last = null;
+                        var notifications = [];
+                        _.each(r, function(o){
+                            var m = moment(o.created_at).startOf('day');
+                            if (!last || !last.isSame(m)) {
+                                last = m;
+                                notifications = [];
+                                $rootScope.allNotifications.push({date: last.toDate(), notifications: notifications});
+                            }
+                            notifications.push(o);
+                        });
                     }).error(function(e){
                        console.log(e);
                     });
