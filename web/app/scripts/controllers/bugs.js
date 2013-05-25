bf8abdee398ee2585d1d4f591c3755da1aefda91
Aishwarya Singhal <asinghal79@gmail.com>
2013-05-25 18:17:06 +0530

    filters et al

--- web/app/scripts/controllers/bugs.js
+++ web/app/scripts/controllers/bugs.js
@@ -3,11 +3,62 @@
 angular.module('webApp')
   .controller('BugsCtrl', function ($rootScope, $scope, sync, $location) {
 
+    $scope.allBugs = [];
     $scope.bug = { user: $rootScope.loggedInUser.id };
+    $scope.owners = [];
+    $scope.statuses = ['New', 'Open', 'Fixed', 'Released', 'Invalid', 'Closed'];
+    $scope.priorities = ['High', 'Medium', 'Low', 'Trivial'];
+    $scope.owner = [];
+    $scope.status = [];
+    $scope.priority = [];
+
+    var applyFilters = function() {
+        $scope.bugs = [];
+        _.each($scope.allBugs, function(o){
+            var match = true;
+            if (match && !isValid('owner', o.owner_id)) match = false;
+            if (match && !isValid('status', o.status)) match = false;
+            if (match && !isValid('priority', o.priority)) match = false;
+
+            if(match) $scope.bugs.push(o);
+        });
+    }
+
+    var addFilter = function(filter, s) {
+        if ($scope[filter].indexOf(s) !== -1) {
+            var index = $scope[filter].indexOf(s);
+            $scope[filter].splice(index, 1);
+        } else {
+            $scope[filter].push(s);
+        }
+        applyFilters();
+    }
+
+    $scope.statusFilter = function(s) {
+        addFilter("status", s);
+    }
+
+    $scope.ownerFilter = function(s) {
+        addFilter("owner", s);
+    }
+
+    $scope.priorityFilter = function(s) {
+        addFilter("priority", s);
+    }
+
+    sync.get("/projects/" + $rootScope.project_id + "/members").success(function(res){
+        var owners = res || [];
+        $scope.owners = _.map(owners, function(o){
+            return o.user;
+        });
+    }).error(function(error){
+        console.log(error);
+    });
 
     var loadBugs = function() {
         sync.get('/projects/' + $rootScope.project_id + '/bugs').success(function(res) {
             $scope.bugs = res || {};
+            $scope.allBugs = $scope.bugs;
         }).error(function() {
             console.log("error");
         });
--- web/app/scripts/controllers/todos.js
+++ web/app/scripts/controllers/todos.js
@@ -3,11 +3,59 @@
 angular.module('webApp')
   .controller('TodosCtrl', function ($rootScope, $scope, sync, $location) {
 
+    $scope.allTodos = [];
     $scope.todo = { user: $rootScope.loggedInUser.id };
+    $scope.owners = [];
+    $scope.statuses = ['Pending', 'Done'];
+    $scope.owner = [];
+    $scope.status = [];
+
+    $scope.toggle = function(todo) {
+        todo.status = (todo.status === "Done") ? "Pending" : "Done";
+    }
+
+    var applyFilters = function() {
+        $scope.todos = [];
+        _.each($scope.allTodos, function(o){
+            var match = true;
+            if (match && !isValid('owner', o.owner_id)) match = false;
+            if (match && !isValid('status', o.status)) match = false;
+
+            if(match) $scope.todos.push(o);
+        });
+    }
+
+    var addFilter = function(filter, s) {
+        if ($scope[filter].indexOf(s) !== -1) {
+            var index = $scope[filter].indexOf(s);
+            $scope[filter].splice(index, 1);
+        } else {
+            $scope[filter].push(s);
+        }
+        applyFilters();
+    }
+
+    $scope.statusFilter = function(s) {
+        addFilter("status", s);
+    }
+
+    $scope.ownerFilter = function(s) {
+        addFilter("owner", s);
+    }
+
+    sync.get("/projects/" + $rootScope.project_id + "/members").success(function(res){
+        var owners = res || [];
+        $scope.owners = _.map(owners, function(o){
+            return o.user;
+        });
+    }).error(function(error){
+        console.log(error);
+    });
 
     var loadTodos = function() {
         sync.get('/projects/' + $rootScope.project_id + '/todos').success(function(res) {
             $scope.todos = res || {};
+            $scope.allTodos = $scope.todos;
         }).error(function() {
             console.log("error");
         });
