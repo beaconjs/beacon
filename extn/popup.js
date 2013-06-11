5bd261f30d21413f69f82f679ef8a233888ae685
Aishwarya Singhal <asinghal79@gmail.com>
2013-06-11 12:47:49 +0530

    add bugs to db

--- extn/popup.js
+++ extn/popup.js
@@ -1,8 +1,15 @@
-var load = function() {
-    chrome.storage.local.get('beacon_auth', function(o){
+var base_url = "http://localhost:3000/";
+var user_id = null;
+
+var bug = {};
+
+var init = function() {
+    chrome.storage.local.get(['beacon_auth', 'beacon_auth_id'], function(o){
         if(o.beacon_auth) {
             $('#logindiv').hide();
             $('#main').show();
+            user_id = o.beacon_auth_id;
+            loadProjects();
         } else {
             $('#logindiv').show();
             $('#main').hide();
@@ -12,25 +19,72 @@ var load = function() {
 
 var capture = function() {
     chrome.tabs.captureVisibleTab(null, {}, function (image) {
-       console.log(image); //the image variable is a base64 encoded image which you should be able to load in either canvas or src attribute of an image.
+       console.log(image);
        $("#main").append("<img src=\"" + image + "\" style=\"height:30em; width:auto;\"/>");
     });
 }
 
 var login = function() {
-    $.post('http://localhost:3000/authenticate', { username: $("#username").val(), password: $("#password").val() }).done(function(res) {
-        console.log(res);
-        chrome.storage.local.set({'beacon_auth': $('#username').val() }, function() {
-            load();
-          });
+    $.post(base_url + 'authenticate', { username: $("#username").val(), password: $("#password").val() }).done(function(res) {
+        if (res) {
+            chrome.storage.local.set({'beacon_auth': $('#username').val(), 'beacon_auth_id': res.id }, function() {
+                init();
+            });
+        }
+    });
+}
+
+var loadProjects = function() {
+    if (user_id) $.get(base_url + 'users/' + user_id + '/projects', function(res) {
+        var options = $("#project");
+        options.html("");
+        if (res) {
+            $.each(res, function() {
+                options.append($("<option />").val(this.project.id).text(this.project.name));
+            });
+
+            loadOwners(res[0].id);
+        }
+    });
+}
+
+var loadOwners = function(project_id) {
+    if (user_id) $.get(base_url + 'projects/' + project_id + '/members', function(res) {
+        var options = $("#owner");
+        options.html("");
+        if (res) {
+            $.each(res, function() {
+                options.append($("<option />").val(this.user.id).text(this.user.name));
+            });
+        }
     });
 }
 
 var logout = function() {
-    chrome.storage.local.remove('beacon_auth', function() {
+    chrome.storage.local.remove(['beacon_auth', 'beacon_auth_id'], function() {
         $("#username").val("");
         $("#password").val("");
-        load();
+        init();
+    });
+}
+
+var save = function() {
+    bug = {
+        title: $("#title").val(),
+        details: $("#details").val(),
+        priority: $("#priority").val(),
+        owner_id: $("#owner").val(),
+        status: 'New',
+        user: user_id
+    }
+    project_id = $('#project').val();
+
+    $.post(base_url + 'projects/' + project_id + '/bugs', bug).done(function(res){
+        bug = {};
+        $("#title").val("");
+        $("#details").val("");
+        $("#priority").val("");
+        $("#owner").val("");
     });
 }
 
@@ -38,7 +92,8 @@ $(document).ready(function() {
     $("#login").click(login);
     $("#logout").click(logout);
     $("#captureBtn").click(capture);
+    $("#saveBtn").click(save);
 });
 
-load();
+init();
 
