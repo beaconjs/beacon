478cd515c2daae65571a067c8fa00ba69bbeea82
Aishwarya Singhal <asinghal79@gmail.com>
2013-06-11 09:34:07 +0530

    basic auth

--- extn/popup.js
+++ extn/popup.js
@@ -1,9 +1,41 @@
 var load = function() {
-    chrome.tabs.captureVisibleTab(null, {}, function (image) {
-       console.log(image); //the image variable is a base64 encoded image which you should be able to load in either canvas or src attribute of an image.
-       document.write("<img src=\"" + image + "\" style=\"height:30em; width:auto;\"/>");
+    chrome.storage.local.get('beacon_auth', function(o){
+        if(o.beacon_auth) {
+            $('#logindiv').hide();
+            $('#main').show();
+        } else {
+            $('#logindiv').show();
+            $('#main').hide();
+        }
     });
 }
+
+var capture = function() {
+    chrome.tabs.captureVisibleTab(null, {}, function (image) {
+       console.log(image); //the image variable is a base64 encoded image which you should be able to load in either canvas or src attribute of an image.
+       $("#main").append("<img src=\"" + image + "\" style=\"height:30em; width:auto;\"/>");
+    });
+}
+
+var login = function() {
+    chrome.storage.local.set({'beacon_auth': $('#username').val() }, function() {
+        load();
+      });
+}
+
+var logout = function() {
+    chrome.storage.local.remove('beacon_auth', function() {
+        $("#username").val("");
+        $("#password").val("");
+        load();
+    });
+}
+
+$(document).ready(function() {
+    $("#login").click(login);
+    $("#logout").click(logout);
+    $("#captureBtn").click(capture);
+});
 
 load();
 
